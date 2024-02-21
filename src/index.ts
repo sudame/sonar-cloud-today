549c89bfeece65df0fb6bd39646a8f5e343aba77
Mikihiro SUDA <mikihiro.suda@chatwork.com>
2024-02-21 10:36:50 +0900

    chore: タイトルを指定する(OGPで表示されるかも)

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,12 @@ function showWarning(message: string): void {
   });
 }
 
+function setTitle(type: "issue" | "activity"): void {
+  document.addEventListener("DOMContentLoaded", () => {
+    document.title = `Sonar Cloudに良い感じにリダイレクトする君 | ${type}`;
+  });
+}
+
 /** Date型を yyyy-mm-dd 形式で返す */
 function toYYYYMMDD(date: Date): string {
   const y = date.getFullYear();
@@ -15,15 +21,6 @@ function toYYYYMMDD(date: Date): string {
 function main(): void {
   const searchParams = new URLSearchParams(window.location.search);
 
-  const id = searchParams.get("id");
-  if (id == null) {
-    showWarning("IDが指定されませんでした");
-    return;
-  }
-  const safeId = id.replace(/[^a-zA-Z0-9\-_]/g, "");
-
-  const offest = parseInt(searchParams.get("offset") ?? "1", 10);
-
   const type = searchParams.get("type");
   if (type == null) {
     showWarning("Typeが指定されませんでした");
@@ -33,6 +30,17 @@ function main(): void {
     showWarning("Typeが不正です");
     return;
   }
+
+  setTitle(type);
+
+  const id = searchParams.get("id");
+  if (id == null) {
+    showWarning("IDが指定されませんでした");
+    return;
+  }
+  const safeId = id.replace(/[^a-zA-Z0-9\-_]/g, "");
+
+  const offest = parseInt(searchParams.get("offset") ?? "1", 10);
 
   const toDate = new Date();
   const fromDate = new Date();
